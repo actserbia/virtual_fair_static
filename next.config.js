ae27e76f68a0ea003830f50cbe57d8fb7ca4447d
vasilie <vasilie.91@gmail.com>
2023-12-18 14:31:57 +0100

    Add new root

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'virtual_fair_static'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 const nextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 }
 
 module.exports = nextConfig
